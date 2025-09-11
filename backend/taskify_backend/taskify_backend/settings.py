df733f3f489e0c5a196af3c08aa6cfddeee359bd
hahuy1708 <hahuy17082005@gmail.com>
2025-09-11 19:34:43 +0700

    1109

--- backend/taskify_backend/taskify_backend/settings.py
+++ backend/taskify_backend/taskify_backend/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'djoser',
+    'taskify_auth',
+    'taskify_core',
 ]
 
 MIDDLEWARE = [
