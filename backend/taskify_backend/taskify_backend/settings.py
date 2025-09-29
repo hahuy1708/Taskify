fb8f48108a5c8467631f5cf54728c6d529ee7f08
hahuy1708 <hahuy17082005@gmail.com>
2025-09-29 19:42:02 +0700

    reset password

--- backend/taskify_backend/taskify_backend/settings.py
+++ backend/taskify_backend/taskify_backend/settings.py
@@ -198,4 +198,11 @@ DJOSER = {
     },
 }
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="smtp.gmail.com")
+EMAIL_PORT = env("EMAIL_PORT", default=587)
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=True)
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
